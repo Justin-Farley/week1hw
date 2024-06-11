c339c19b06ebdcd6bfe69e83a4bd9d44e4aa31af
Justin-Farley <farleyjc0612@gmail.com>
2024-06-11 05:03:43 -0400

    adding files

---
+++ homework.py
@@ -0,0 +1,40 @@
+weather = "sunny!"
+
+weather == "sunny!"
+print("wear sunglasses")
+print(weather)
+
+weather = "Rainy."
+print("Take an umbrella")
+print(weather)
+
+
+user = input('How are you feeling?\n')
+print(user)
+happy = input('I love to hear that!')
+print(happy)
+sad = input('I hope your day gets better')
+print(sad)
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+
+bread = 2.75
+peanut_butter = 4.50
+jelly = 3.23
+total = (bread + peanut_butter + jelly)
+print(total)
+
+
+
+
+
+
