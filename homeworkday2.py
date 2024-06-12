731bf98e8e4bdd81b1e6f7c3fe7387286378324e
Justin-Farley <farleyjc0612@gmail.com>
2024-06-12 02:37:07 -0400

    Add files via upload

---
+++ homeworkday2.py
@@ -0,0 +1,27 @@
+# number = int(input("enter a number"))
+# if number > 0:
+#     print("The number is positive.")
+# elif number == 0:
+#     print("The number is zero.")
+# else:
+#     print("The number is negative.")
+
+
+num1 = 4
+num2 = 20
+num3 = 11
+if (num1 >= num2) and (num1 >= num3):
+    largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+    largest = num2
+else:
+    largest = num3
+print("The Largest Number is", largest)
+
+
+
+    
+    
+
+
+
